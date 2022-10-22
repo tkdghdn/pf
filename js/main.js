3ceed856cc530ef00bbc96c14b7d270b283b3682
LeeSanghoTCK <110366619+LeeSanghoTCK@users.noreply.github.com>
2022-10-22 21:11:25 +0900

    19_promotion_img_slide

--- js/main.js
+++ js/main.js
@@ -54,3 +54,22 @@ new Swiper('.notice-line .swiper-container',{
     autoplay: true,
     loop: true,
 });
+
+//swiper promotion img
+new Swiper('.promotion .swiper-container',{
+    slidesPerView: 3, //한번에 보여줄 슬라이드 수
+    spacebetween: 10, //슬라이드 사이 여백
+    centeredSlides: true, //1번슬라이드가 가운데 보이기
+    loop: true,
+    /* autoplay: {
+        delay: 5000
+    } */
+    pagination:{
+        el: '.promotion .swiper-pagination',//페이지 번호 요소 선택자
+        clickable: true,
+    },
+    navigation:{
+        prevEl:'.promotion .swiper-prev',
+        nextEl:'.promotion .swiper-next',
+    }
+});
