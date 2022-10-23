655f67822f358b0d85f4f993f67bf95a7ae83139
LeeSanghoTCK <110366619+LeeSanghoTCK@users.noreply.github.com>
2022-10-23 21:29:23 +0900

    28_scrollMagic_animation_1

--- js/main.js
+++ js/main.js
@@ -112,3 +112,15 @@ function floatingObject(selector,delay, size){
 floatingObject('.floating1', 1, 15);
 floatingObject('.floating2', .5, 15);
 floatingObject('.floating3', 1.5, 20);
+
+//scrollMagic
+const spyEls = document.querySelectorAll('section.scroll-spy');
+spyEls.forEach(function(spyEl){
+    new ScrollMagic
+        .Scene({
+            triggerElement: spyEl, //보여짐 여부를 감시할 요소를 지정
+            triggerHook: .8,
+        })
+        .setClassToggle()
+        .addTo();//특정요소 감시하는 옵션지정
+});
